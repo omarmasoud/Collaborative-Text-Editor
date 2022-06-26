7824dc430217e20b728e9664e40d8e3275c3092d
omarmasoud <omark897@gmail.com>
2022-06-26 17:09:14 +0200

    removed previous additions in setup project batch file

--- setup_project.bat
+++ setup_project.bat
@@ -3,8 +3,6 @@ ECHO setting up virtual python environment
 python -m venv venv
 ECHO installing requirements.txt
 venv\scripts\python.exe -m pip install -r requirements.txt
-ECHO installing ttkbootstrap
+ECHO installing ttkbootstrap ,websocket library , awscli
 venv\scripts\python.exe -m pip install -e .
 ECHO ttkbootstrap environment setup is complete
-venv\scripts\python.exe -m pip install awscli .
-ECHO AWS CLI environment setup is complete
